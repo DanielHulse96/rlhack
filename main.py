a1a4b0348f43a775c115c589178ba5788743fdc7
Daniel Hulse <daniel.3.hulse@bt.com>
2025-10-22 16:50:17 +0100

    Add testing branch and remove .idea files

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import ale_py
 import gymnasium as gym
+import numpy as np
 from tqdm import tqdm
 
 from LearningVisualisation import LearningVisualisation
 from QLearningAgent import QLearningAgent
 
 if __name__ == '__main__':
+    # True to train, False to test
+    training_mode = True
+
+    # parameters for the agent
     learning_rate = 0.01  # How fast to learn (higher = faster but less stable)
     n_episodes = 100_000  # Number of episodes
     start_epsilon = 1.0  # Start with 100% random actions
@@ -15,6 +20,9 @@ if __name__ == '__main__':
     # Create the environment
     env = gym.make("ALE/Tetris-v5", render_mode="human", obs_type="grayscale")
     env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
+
+    env.metadata["render_fps"] = 30
+    print(f"Using following environment settings: {env.metadata}")
 
     # For visualisation:
     visualisation = LearningVisualisation(env, 500)
@@ -27,8 +35,6 @@ if __name__ == '__main__':
         epsilon_decay=epsilon_decay,
         final_epsilon=final_epsilon,
     )
-
-    print(f"Using following environment settings: {env.metadata}")
 
     '''
         For Tetris-v5 the action Space is Discrete(5) with the following actions
@@ -47,18 +53,30 @@ if __name__ == '__main__':
     '''
     print(f"Observation space: {env.observation_space}")
 
+    old_epsilon = agent.epsilon
+    total_rewards = []
+    if training_mode:
+        print("Running training mode...")
+    else:
+        print("Running in testing mode...")
+        # If testing, temporarily disable exploration - just pure exploitation
+        agent.epsilon = 0.0
+
     for episode in tqdm(range(n_episodes)):
         # Start a new game
         obs, info = env.reset()
 
-        end_of_episode = False
+        # Keep track of how many rewards, for testing
+        episode_reward = 0
 
+        end_of_episode = False
         while not end_of_episode:
             # Agent chooses an action (initially random, gradually improves)
             action = agent.get_action(obs)
 
             # Take the action and observe the result
             next_obs, reward, terminated, truncated, info = env.step(action)
+            episode_reward += reward
 
             # Learn from this experience
             agent.update(obs, action, reward, terminated, next_obs)
@@ -67,10 +85,23 @@ if __name__ == '__main__':
             end_of_episode = terminated or truncated
             obs = next_obs
 
-        # Reduce the exploration rate (so the agent can become less random over time)
-        agent.decay_epsilon()
+        if training_mode:
+            # Reduce the exploration rate (so the agent can become less random over time)
+            agent.decay_epsilon()
+        else:
+            total_rewards.append(episode_reward)
 
         # Update graphs
         visualisation.update(agent)
+
+    if training_mode:
+        print(f"Finished training for {n_episodes} episodes")
+    else:
+        # For testing, restore old epsilon and output some metrics
+        agent.epsilon = old_epsilon
+        average_reward = np.mean(total_rewards)
+        print(f"Test Results over {n_episodes} episodes:")
+        print(f"Average Reward: {average_reward:.3f}")
+        print(f"Standard Deviation: {np.std(total_rewards):.3f}")
 
     env.close()
